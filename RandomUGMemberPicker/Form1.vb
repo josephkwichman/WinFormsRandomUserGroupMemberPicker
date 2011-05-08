84956d073e477b45978c0df1a8d24ea0c3701adf
jkw5664 <josephkwichman@yahoo.com>
2011-05-08 06:57:08 -0500

    First Commit for Project

---
+++ RandomUGMemberPicker/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Public MemberArrayList As New ArrayList
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim NewestMember As String
+        NewestMember = TextBox1.Text.ToString()
+        MemberArrayList.Add(NewestMember)
+        TextBox1.Clear()
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If MemberArrayList.Count > 0 Then
+            Dim randomInt As New Random
+            Dim winnerName As String
+            winnerName = MemberArrayList.Item(randomInt.Next(0, MemberArrayList.Count))
+            Label2.Text = winnerName
+            MemberArrayList.Remove(winnerName)
+        End If
+
+    End Sub
+End Class
